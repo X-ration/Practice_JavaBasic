62efe121cce877e8ddc507046c13f075c750a804
adam <starksmile@hotmail.com>
2018-03-06 14:16:29 +0800

    Simple factory pattern.

---
+++ src/com/adam/factory/simpleFactoryPattern/CircleShape.java
@@ -0,0 +1,13 @@
+package com.adam.factory.simpleFactoryPattern;
+
+/**
+ * @author adam
+ * 创建于 2018-03-06 14:07.
+ * 圆形。
+ */
+public class CircleShape implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("CircleShape.draw()");
+    }
+}
---
+++ src/com/adam/factory/simpleFactoryPattern/Shape.java
@@ -0,0 +1,12 @@
+package com.adam.factory.simpleFactoryPattern;
+
+/**
+ * @author adam
+ * 创建于 2018-03-06 14:06.
+ * 抽象图形接口。
+ */
+public interface Shape {
+
+    void draw();
+
+}
